eac9f5fd0ebad492a661667ae59bb2dbdfd202ac
javi <enriq018coding@gmail.com>
2017-11-18 20:20:49 -0800

    added main server routes

--- server/index.js
+++ server/index.js
@@ -1,29 +1,81 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
+
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
 // var items = require('../database-mongo');
 
+//*********************************************************************************************************
+//DATABASE HELPER FUNCTIONS NEEDED: 
+// searchTerm(string, callback) - Gets all recipies from db that include string in its name then uses cb to return them.  
+// addToDb(obj, callback) - Adds obj to db then invokes cb. 
+// getAll(callback) - Gets all recipes from database and uses cb to return them
+// getUserFavorites(userId, callback) - Users userId to find users saved recipes then returns them using cb.
+//*********************************************************************************************************
+
 var app = express();
 
-// UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(bodyParser.json())
 
-// UNCOMMENT FOR ANGULAR
-// app.use(express.static(__dirname + '/../angular-client'));
-// app.use(express.static(__dirname + '/../node_modules'));
+app.post('/search', (req, res) => {
+// 	/search - Query DB for recipes by search term	
+  db.searchTerm(req.body.search, (data) => {
+  	res.status(201);
+  	res.send(data);
+  })
+})
+	
+app.get('/search/filter', (req, res) => {
+// /search/filter - GET request to API (ingredients, ??)
+// fill out options with correct api info 
+  var options = {
+  }
+  //call api
+	request(options, (error, response, body) => {
+		if(error){
+			console.log(error)
+			res.send(error)
+		} else {
+			//add api info to db then send back to client 
+			db.addToDb(body, (data)=> {
+				res.status(200);
+				res.send(body)
+			})
+		}
+	})
+})
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+app.get('/grid', (req,res) => {
+// 	/grid - Query DB for all recipes (12?)
+	db.getAll(data => {
+		res.status(200);
+		res.send(data);
+	})
+})
+
+app.get('/favorites', (req,res) => {
+// 	/favorites - Query DB for user’s favorites
+	//either use url to find specific user, or send user info in header, or convert to 
+	//post and send user name in body 
+	db.getUserFavorites(userId, (data)=> {
+		res.status(200);
+		res.send(data)
+	})
+})
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
 });
+
+	
+	
+
+
+
+	
+
+
+
 
