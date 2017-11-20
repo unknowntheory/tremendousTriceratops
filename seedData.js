157d2c3be07e7e7324711770feabc82aaffd3f6c
Daniel London <theryy408@gmail.com>
2017-11-20 15:36:19 -0800

    (test) added sample data

---
+++ seedData.js
@@ -0,0 +1,159 @@
+
+const sampleData = [
+  {
+    "q": "chicken,broccoli",
+    "from": 0,
+    "to": 10,
+    "params": {
+        "sane": [],
+        "q": [
+            "chicken,broccoli"
+        ],
+        "app_key": [
+            "06a6e4be0f23cd75db3af16ae7d7b696"
+        ],
+        "app_id": [
+            "d6c2df4e"
+        ]
+    },
+    "more": true,
+    "count": 3414,
+    "hits": [
+        {
+            "recipe": {
+                "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_9c74edcd1bfec75f258dd6f0fc91bcad",
+                "label": "Chicken Broccoli Stir Fry",
+                "image": "https://www.edamam.com/web-img/6f9/6f98fb5f7317b02d22b79b2bce969505.jpg",
+                "source": "Food52",
+                "url": "https://food52.com/recipes/4736-chicken-broccoli-stir-fry",
+                "shareAs": "http://www.edamam.com/recipe/chicken-broccoli-stir-fry-9c74edcd1bfec75f258dd6f0fc91bcad/chicken%2Cbroccoli",
+                "yield": 4,
+                "dietLabels": [],
+                "healthLabels": [
+                    "Peanut-Free",
+                    "Tree-Nut-Free",
+                    "Alcohol-Free"
+                ],
+                "cautions": [],
+                "ingredientLines": [
+                    "4 servings of cooked rice (i used royal blend)",
+                    "1 tablespoon canola oil",
+                    "1 cup sliced fresh mushrooms",
+                    "4-5 cups broccoli florets",
+                    "1 cup jicama, rough chopped",
+                    "1/2 cup carrots, thinly sliced",
+                    "1/2 cup celery, rough chopped",
+                    "4-5 scallions, thinly sliced",
+                    "2-3 cups shredded cooked chicken",
+                    "15 abola peppers"
+                ],
+
+                "calories": 2083.076674804687,
+              },
+
+            "recipe": {
+                "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_d22e8065ee886b6a9c953c3ab8f6a01c",
+                "label": "Chicken & Broccoli Pasta Bake",
+                "image": "https://www.edamam.com/web-img/e55/e5544cb9ddeca67b2d6141f35048987b.jpg",
+                "source": "BBC Good Food",
+                "url": "http://www.bbcgoodfood.com/recipes/1948/",
+                "shareAs": "http://www.edamam.com/recipe/chicken-broccoli-pasta-bake-d22e8065ee886b6a9c953c3ab8f6a01c/chicken%2Cbroccoli",
+                "yield": 4,
+                "dietLabels": [],
+                "healthLabels": [
+                    "Peanut-Free",
+                    "Alcohol-Free"
+                ],
+                "cautions": [],
+                "ingredientLines": [
+                    "50.0g flaked almonds",
+                    "2.0 tbsp olive oil",
+                    "175.0g chestnut mushrooms , quartered",
+                    "bunch of spring onions , finely sliced",
+                    "284 ml carton single cream",
+                    "200.0g broccoli , cut into very small florets and the stems thinly sliced",
+                    "4.0 tbsp sundried tomato paste",
+                    "1 garlic clove , finely chopped",
+                    "85.0g mature cheddar , grated",
+                    "350.0g pasta shells or quills",
+                    "350.0g boneless, skinless chicken breasts, thinly sliced",
+                    "FOR THE TOPPING",
+                    "80 g pkt soft cheese with garlic and herbs (such as Boursin)"
+                ],
+
+                "calories": 4118.693181457519,
+              },
+
+            "recipe": {
+                "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_f05e519e5df738f87ca68b209e67dfd6",
+                "label": "Chicken, Broccoli & Brown Rice Casserole",
+                "image": "https://www.edamam.com/web-img/3fa/3fa6245cec00cc11a19f947f71e4a64a.jpg",
+                "source": "The Kitchn",
+                "url": "http://www.thekitchn.com/recipe-chicken-broccoli-amp-brown-rice-casserole-recipes-from-the-kitchn-213300",
+                "shareAs": "http://www.edamam.com/recipe/chicken-broccoli-brown-rice-casserole-f05e519e5df738f87ca68b209e67dfd6/chicken%2Cbroccoli",
+                "yield": 6,
+                "dietLabels": [],
+                "healthLabels": [
+                    "Peanut-Free",
+                    "Alcohol-Free"
+                ],
+                "cautions": [
+                    "Gluten",
+                    "Wheat"
+                ],
+                "ingredientLines": [
+                    "3 cups homemade or low-sodium/unsalted chicken stock (or water)",
+                    "1 cup brown rice",
+                    "12 ounces broccoli florets (about 4 cups)",
+                    "2 cups nonfat or low-fat cow's milk (Lactose-free cow's milk optional)",
+                    "1/4 cup all-purpose flour",
+                    "1 cup grated Cheddar cheese",
+                    "1 cup nonfat or low-fat Greek Yogurt",
+                    "Salt and freshly ground pepper, to taste (optional)",
+                    "3 cups shredded chicken (from 1 rotisserie chicken)",
+                    "1/4 cup sliced almonds",
+                    "1/4 cup bread crumbs",
+                    "1 tablespoons olive oil"
+                ],
+
+                "calories": 3230.8900000000003,
+              },
+
+            "recipe": {
+                "uri": "http://www.edamam.com/ontologies/edamam.owl#recipe_48ca1624ab897a0cd979880623ede1e2",
+                "label": "Chicken, Broccoli, And Lemon Stir-Fry",
+                "image": "https://www.edamam.com/web-img/bbd/bbd1b736e529f540ec918c0d809d2331.jpg",
+                "source": "Martha Stewart",
+                "url": "http://www.marthastewart.com/340870/chicken-broccoli-and-lemon-stir-fry",
+                "shareAs": "http://www.edamam.com/recipe/chicken-broccoli-and-lemon-stir-fry-48ca1624ab897a0cd979880623ede1e2/chicken%2Cbroccoli",
+                "yield": 4,
+                "dietLabels": [],
+                "healthLabels": [
+                    "Peanut-Free",
+                    "Tree-Nut-Free",
+                    "Alcohol-Free"
+                ],
+                "cautions": [
+                    "Gluten",
+                    "Wheat"
+                ],
+                "ingredientLines": [
+                    "3 tablespoons soy sauce",
+                    "2 tablespoons rice vinegar",
+                    "2 tablespoons honey",
+                    "2 cloves garlic, minced",
+                    "1/4 to 1/2 teaspoon crushed red pepper",
+                    "1 1/2 pounds boneless, skinless chicken breasts, cut crosswise into 1/2-inch-thick slices",
+                    "2 tablespoons canola oil",
+                    "1 1/2 pounds broccoli, stems peeled and sliced 1/4 inch thick, florets separated",
+                    "1 small lemon, halved lengthwise and thinly sliced crosswise, seeds removed",
+                    "2 teaspoons cornstarch, dissolved in 1 tablespoon water",
+                    "2 tablespoons toasted sesame seeds"
+                ],
+
+                "calories": 1600.4558658730987,
+              }
+            }
+          ]
+        }
+]
